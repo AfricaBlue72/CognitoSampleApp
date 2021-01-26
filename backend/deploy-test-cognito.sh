e27bcd0313bab66766c5acb1bfde7498f0f8423b
AfricaBlue72 <akrijten@gmail.com>
2021-01-26 19:39:15 +0100

    Added backend files

---
+++ backend/deploy-test-cognito.sh
@@ -0,0 +1,47 @@
+﻿#!/bin/sh
+
+#./deploy.sh 
+REGION='eu-west-1'
+
+delete=false
+deploy=true
+dryrun=false
+verbose=false
+
+run() {
+	${verbose} && echo "${@}"
+	${dryrun} || "${@}"
+}
+
+while getopts "d" arg
+do
+        case "${arg}" in
+        d)
+                delete=true
+                deploy=false
+                ;;
+	esac
+done
+
+
+#Start the deployment
+echo "##########################################################################"
+
+if(${deploy})
+then
+    echo 'Deploying Managed AD'
+    cfn-create-or-update --stack-name test-congito --template-body file://test-cognito.yml \
+    --parameters \
+    ParameterKey=UserPoolName,ParameterValue=TestUserPool \
+    ParameterKey=IdentityPoolName,ParameterValue=TestIdentityPool \
+    --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+    --region ${REGION} --wait
+fi
+
+if(${delete})
+then
+    echo 'Delete Cognito'
+    aws cloudformation delete-stack --stack-name test-congito || continue 
+    aws cloudformation wait stack-delete-complete --stack-name test-cognito
+fi
+ 
